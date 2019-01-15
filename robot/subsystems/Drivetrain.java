78599057f7f3c64719a647caaefc0766741981e3
3204robotics <46610816+3204robotics@users.noreply.github.com>
2019-01-15 17:48:35 -0500

    Add files via upload

---
+++ robot/subsystems/Drivetrain.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj.command.Subsystem;
+//import org.usfirst.frc.team1.robot.RobotMap;
+import edu.wpi.first.wpilibj.GenericHID;
+
+public class Drivetrain extends Subsystem {
+
+
+
+public void initDefaultCommand(){
+
+}
+}
